cbc0eba1cb9c0e3f795c89f3061731b81889f338
Trevor Fehrman <trevorfehrman@gmail.com>
2019-04-05 01:07:26 -0700

    extracted timeMap

--- components/Timeline.js
+++ components/Timeline.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 
+import { timeMap } from '../utils/timeMap';
+
 const useStyles = makeStyles({
   timeline: {
     display: 'flex',
-    flexDirection: 'column',
     flexWrap: 'wrap',
+    flexDirection: 'column',
     alignContent: 'center',
-    height: '200px'
+    height: '205px'
   },
   box: {
     height: '90px',
@@ -24,27 +26,6 @@ const useStyles = makeStyles({
     backgroundColor: 'blue'
   }
 });
-
-const timeMap = [
-  '8:00AM',
-  '8:30AM',
-  '9:00AM',
-  '9:30AM',
-  '10:00AM',
-  '10:30AM',
-  '11:00AM',
-  '11:30AM',
-  '12:00PM',
-  '12:30PM',
-  '1:00PM',
-  '1:30PM',
-  '2:00PM',
-  '2:30PM',
-  '3:00PM',
-  '3:30PM',
-  '4:00PM',
-  '4:30PM'
-];
 
 export default function Timeline({ slots }) {
   console.log(slots);
