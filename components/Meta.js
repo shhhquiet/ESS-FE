0523872914dcd08e4d495a22a2abc9f2b267a3b0
Lauren <llwor94@gmail.com>
2019-04-05 22:51:42 -0700

    wooo things r set up kinda

---
+++ components/Meta.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+const Meta = () => (
+	<Head>
+		<meta name='viewport' content='width=device-width, initial-scale=1' charSet='utf-8' />
+		<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700,200' rel='stylesheet' />
+		<title>Eastside Swim School</title>
+	</Head>
+);
+
+export default Meta;
---
+++ utils/getPageContext.js
@@ -0,0 +1,44 @@
+import { SheetsRegistry } from 'jss';
+import { createMuiTheme, createGenerateClassName } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			main: '#2979ff',
+		},
+		secondary: {
+			main: '#a7ffea',
+		},
+	},
+	typography: {
+		fontFamily: [ 'Montserrat', 'sans-serif' ].join(','),
+		useNextVariants: true,
+	},
+});
+
+export function createPageContext() {
+	return {
+		theme,
+		// This is needed in order to deduplicate the injection of CSS in the page.
+		sheetsManager: new Map(),
+		// This is needed in order to inject the critical CSS.
+		sheetsRegistry: new SheetsRegistry(),
+		// The standard class name generator.
+		generateClassName: createGenerateClassName(),
+	};
+}
+
+export default function getPageContext() {
+	// Make sure to create a new context for every server-side request so that data
+	// isn't shared between connections (which would be bad).
+	if (!process.browser) {
+		return createPageContext();
+	}
+
+	// Reuse context on the client-side.
+	if (!global.__INIT_MATERIAL_UI__) {
+		global.__INIT_MATERIAL_UI__ = createPageContext();
+	}
+
+	return global.__INIT_MATERIAL_UI__;
+}
