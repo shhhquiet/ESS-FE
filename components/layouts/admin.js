dbdf767dabbb8a948818bc0a3196f55621477272
Lauren <llwor94@gmail.com>
2019-05-09 21:56:39 -0700

    added some icons

--- components/layouts/admin.js
+++ components/layouts/admin.js
@@ -1,15 +1,25 @@
+import React, {useState} from 'react';
 import {useQuery} from 'react-apollo-hooks';
 import NavBar from '../../MUI-Components/admin-components/Navbars/AdminNavbar';
 import Sidebar from '../../MUI-Components/admin-components/Sidebar/Sidebar';
 import withStyles from '@material-ui/core/styles/withStyles';
-import { CalendarToday } from '@material-ui/icons';
+import { CalendarToday, Dashboard, StarRate, Receipt } from '@material-ui/icons';
+import cx from "classnames";
 import {CURRENT_USER_QUERY} from '../../gql/Queries/User'
 import style from '../../static/jss/layouts/adminStyle';
 
 
 const Layout = ({ classes, children }) => {
 	const {data} = useQuery(CURRENT_USER_QUERY)
+	const [mobileOpen, setMobileOpen] = useState(false)
+	const [miniActive, setMiniActive] = useState(false)
 	console.log(data)
+	const mainPanel =
+      classes.mainPanel +
+      " " +
+      cx({
+				[classes.mainPanelSidebarMini]: miniActive
+			})
 	return (
 		
 		<div className={classes.wrapper}>
@@ -22,9 +32,10 @@ const Layout = ({ classes, children }) => {
 				userImg={data.currentUser.imageURL}
 				logo='/static/admin-styles/img/logo.png'
 				logoText='Eastside Swim School'
+				miniActive={miniActive}
 			/>
-			<div className={classes.mainPanel}>
-				<NavBar />
+			<div className={mainPanel}>
+				<NavBar miniActive={miniActive} sidebarMinimize={() => setMiniActive(!miniActive)}/>
 				<div className={classes.content}>
 					<div className={classes.container}>{children}</div>
 				</div>
@@ -40,13 +51,14 @@ var dashRoutes = [
 	{
 		path: '/dashboard',
 		name: 'Dashboard',
-		//icon: DashboardIcon,
+		icon: Dashboard,
 		// component: Dashboard,
 		layout: '/admin',
 	},
 	{
 		path: '/schedule',
 		name: 'Schedule',
+		icon: CalendarToday,
 		layout: '/admin',
 	},
 	{
@@ -62,11 +74,13 @@ var dashRoutes = [
 	{
 		path: '/reports',
 		name: 'Reports',
+		icon: Receipt,
 		layout: '/admin',
 	},
 	{
 		path: '/reviews',
 		name: 'Reviews',
+		icon: StarRate,
 		layout: '/admin',
 	},
 ];
