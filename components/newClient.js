ef57cc053cd1f76346c4dc50db5316c9c9451795
Lauren <llwor94@gmail.com>
2019-05-11 20:13:27 -0700

    created new client

--- components/newClient.js
+++ components/newClient.js
@@ -1,5 +1,6 @@
 import Dialog from "@material-ui/core/Dialog";
 import DialogContent from "@material-ui/core/DialogContent";
+import Wizard from '../MUI-Components/admin-components/Wizard/Wizard'
 
 const NewClient = ({open, handleClose}) => {
   return (
@@ -9,9 +10,24 @@ const NewClient = ({open, handleClose}) => {
       onClose={handleClose}
       aria-labelledby='max-width-dialog-title'
     >
-      <DialogContent>o hi</DialogContent>
+      <DialogContent><Wizard steps={[
+              { stepName: "About", stepComponent: Step1, stepId: "about" },{ stepName: "Account", stepComponent: Step2, stepId: "account" },
+              { stepName: "Address", stepComponent: Step3, stepId: "address" }]} /></DialogContent>
     </Dialog>
   );
 };
+
+const Step1 = () => (
+  <div>hoooo</div>
+)
+
+const Step2 = () => (
+  <div>heyyy</div>
+)
+
+const Step3 = () => (
+  <div>yoooo</div>
+)
+
 
 export default NewClient;
