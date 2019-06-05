3f9de2d8d92ad750058c360d13acb2a551ff4f34
Lauren <llwor94@gmail.com>
2019-06-04 22:05:47 -0700

    fixed mui setup

--- components/layouts/admin.js
+++ components/layouts/admin.js
@@ -1,27 +1,50 @@
-import React, {useState} from 'react';
-import {useQuery} from 'react-apollo-hooks';
-import { withRouter } from 'next/router';
-import clsx from 'clsx';
-import {AppBar, Toolbar, IconButton, Typography, Drawer, Divider, List, ListItem, ListItemIcon, ListItemText} from '@material-ui/core'
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import NewClient from '../newClient'
-// import NavBar from '../../MUI-Components/admin-components/Navbars/AdminNavbar';
-// import Sidebar from '../../MUI-Components/admin-components/Sidebar/Sidebar';
-//import withStyles from '@material-ui/core/styles/withStyles';
-import { CalendarToday, Dashboard, Stars, Receipt, SupervisorAccount, Face, Menu, ChevronLeft, ChevronRight, Inbox, Mail } from '@material-ui/icons';
-import cx from "classnames";
-import {CURRENT_USER_QUERY} from '../../gql/Queries/User'
-//import style from '../../static/jss/layouts/adminStyle';
+import React, {useState} from "react";
+import {useQuery} from "react-apollo-hooks";
+import {withRouter} from "next/router";
+import Link from "next/link";
+import clsx from "clsx";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Drawer,
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListItemAvatar,
+  Avatar,
+} from "@material-ui/core";
+import {makeStyles, useTheme} from "@material-ui/core/styles";
+import NewClient from "../newClient";
+
+import {
+  CalendarToday,
+  Dashboard,
+  Stars,
+  Receipt,
+  SupervisorAccount,
+  Face,
+  Menu,
+  ChevronLeft,
+  ChevronRight,
+  Inbox,
+  Mail,
+} from "@material-ui/icons";
+
+import {CURRENT_USER_QUERY} from "../../gql/Queries/User";
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
+    display: "flex",
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
@@ -29,7 +52,7 @@ const useStyles = makeStyles(theme => ({
   appBarShift: {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -38,87 +61,92 @@ const useStyles = makeStyles(theme => ({
     marginRight: 36,
   },
   hide: {
-    display: 'none',
+    display: "none",
   },
   drawer: {
     width: drawerWidth,
     flexShrink: 0,
-    whiteSpace: 'nowrap',
+    whiteSpace: "nowrap",
   },
   drawerOpen: {
     width: drawerWidth,
-    transition: theme.transitions.create('width', {
+    transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
   },
   drawerClose: {
-    transition: theme.transitions.create('width', {
+    transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
-    overflowX: 'hidden',
+    overflowX: "hidden",
     width: theme.spacing(7) + 1,
-    [theme.breakpoints.up('sm')]: {
+    [theme.breakpoints.up("sm")]: {
       width: theme.spacing(9) + 1,
     },
   },
   toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: "0 8px",
     ...theme.mixins.toolbar,
   },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
   },
+  toolbarInfo: {
+    display: "flex",
+    alignItems: "center",
+  },
+  avatar: {
+    marginRight: "5px",
+  },
 }));
 
-
-const Layout = ({ children, router }) => {
-	const classes = useStyles();
+const Layout = ({children, router}) => {
+  const classes = useStyles();
   const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-	const {data} = useQuery(CURRENT_USER_QUERY)
-	console.log(theme)
-	function handleDrawerOpen() {
+  const [open, setOpen] = useState(false);
+  const {data} = useQuery(CURRENT_USER_QUERY);
+
+  function handleDrawerOpen() {
     setOpen(true);
   }
 
   function handleDrawerClose() {
     setOpen(false);
   }
-	
-	return (
-		
-		<div className={classes.root}>
-		 <AppBar
-        position="fixed"
+
+  return (
+    <div className={classes.root}>
+      <AppBar
+        position='fixed'
         className={clsx(classes.appBar, {
           [classes.appBarShift]: open,
         })}
       >
         <Toolbar>
           <IconButton
-            color="inherit"
-            aria-label="Open drawer"
+            color='inherit'
+            aria-label='Open drawer'
             onClick={handleDrawerOpen}
-            edge="start"
+            edge='start'
             className={clsx(classes.menuButton, {
               [classes.hide]: open,
             })}
           >
             <Menu />
           </IconButton>
-          <Typography variant="h6" noWrap>
-            Mini variant drawer
+          <Typography variant='h6' noWrap>
+            Eastside Swim School
           </Typography>
         </Toolbar>
       </AppBar>
       <Drawer
-        variant="permanent"
+        variant='permanent'
         className={clsx(classes.drawer, {
           [classes.drawerOpen]: open,
           [classes.drawerClose]: !open,
@@ -133,33 +161,38 @@ const Layout = ({ children, router }) => {
       >
         <div className={classes.toolbar}>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRight /> : <ChevronLeft />}
+            {theme.direction === "rtl" ? <ChevronRight /> : <ChevronLeft />}
           </IconButton>
         </div>
-        {/* <Divider />
+        <Divider />
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <Inbox /> : <Mail />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List> */}
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar src={data.currentUser.imageURL} />
+            </ListItemAvatar>
+            <ListItemText primary={data.currentUser.firstName} />
+          </ListItem>
+        </List>
+        
         <Divider />
         <List>
           {dashRoutes.map((route, index) => (
-            <ListItem button key={route.name}>
-              <ListItemIcon><route.icon /></ListItemIcon>
-              <ListItemText primary={route.name} />
-            </ListItem>
+            <Link href={`/admin${route.path}`}>
+              <ListItem button key={route.name} selected={router.pathname === `/admin${route.path}`}>
+                <ListItemIcon>
+                  <route.icon />
+                </ListItemIcon>
+                <ListItemText primary={route.name} />
+              </ListItem>
+            </Link>
           ))}
         </List>
       </Drawer>
-			<main className={classes.content}>
+      <main className={classes.content}>
         <div className={classes.toolbar} />
-       {children}
+        {children}
       </main>
-			{/* <Sidebar
+      {/* <Sidebar
 				routes={dashRoutes}
 				bgColor='blue'
 				color='white'
@@ -177,49 +210,48 @@ const Layout = ({ children, router }) => {
 				</div>
 			</div>
 			<NewClient open={newClientOpen} handleClose={() => setNewClientOpen(false)}/> */}
-		</div>
-	
-	);
+    </div>
+  );
 };
 
 export default withRouter(Layout);
 
 var dashRoutes = [
-	{
-		path: '/dashboard',
-		name: 'Dashboard',
-		icon: Dashboard,
-		// component: Dashboard,
-		layout: '/admin',
-	},
-	{
-		path: '/schedule',
-		name: 'Schedule',
-		icon: CalendarToday,
-		layout: '/admin',
-	},
-	{
-		path: '/clients',
-		name: 'Clients',
-		icon: Face,
-		layout: '/admin',
-	},
-	{
-		path: '/instructors',
-		name: 'Instructors',
-		icon: SupervisorAccount,
-		layout: '/admin',
-	},
-	{
-		path: '/reports',
-		name: 'Reports',
-		icon: Receipt,
-		layout: '/admin',
-	},
-	{
-		path: '/reviews',
-		name: 'Reviews',
-		icon: Stars,
-		layout: '/admin',
-	},
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: Dashboard,
+    // component: Dashboard,
+    layout: "/admin",
+  },
+  {
+    path: "/schedule",
+    name: "Schedule",
+    icon: CalendarToday,
+    layout: "/admin",
+  },
+  {
+    path: "/clients",
+    name: "Clients",
+    icon: Face,
+    layout: "/admin",
+  },
+  {
+    path: "/instructors",
+    name: "Instructors",
+    icon: SupervisorAccount,
+    layout: "/admin",
+  },
+  {
+    path: "/reports",
+    name: "Reports",
+    icon: Receipt,
+    layout: "/admin",
+  },
+  {
+    path: "/reviews",
+    name: "Reviews",
+    icon: Stars,
+    layout: "/admin",
+  },
 ];
--- pages/_document.js
+++ pages/_document.js
@@ -1,40 +1,41 @@
-import React from 'react';
-import Document, { Head, Main, NextScript } from 'next/document';
-import flush from 'styled-jsx/server';
+import React from "react";
+import Document, {Head, Main, NextScript} from "next/document";
+import {ServerStyleSheets} from "@material-ui/styles";
+import flush from "styled-jsx/server";
 
 export default class MyDocument extends Document {
-	static getInitialProps({ renderPage }) {
-		let pageContext;
-		const page = renderPage(App => props => {
-			pageContext = props.pageContext;
-			return <App {...props} />;
-		});
-		let css;
-
-		if (pageContext) {
-			css = pageContext.sheetsRegistry.toString();
-		}
-		return {
-			...page,
-			pageContext,
-			styles: (
-				<React.Fragment>
-					<style id='jss-server-side' dangerouslySetInnerHTML={{ __html: css }} />
-					{flush() || null}
-				</React.Fragment>
-			),
-		};
-	}
-
-	render() {
-		return (
-			<html>
-				<Head />
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</html>
-		);
-	}
+  render() {
+    return (
+      <html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: App => props => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: (
+      <React.Fragment>
+        {sheets.getStyleElement()}
+        {flush() || null}
+      </React.Fragment>
+    ),
+  };
+};
